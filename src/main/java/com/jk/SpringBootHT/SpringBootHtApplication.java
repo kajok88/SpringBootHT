5f75f08d9ac82fdee8b60271b5d04f080e7d00b3
kajok88 <skyborgg88@gmail.com>
2023-12-05 18:49:25 +0200

    Alustus

---
+++ src/main/java/com/jk/SpringBootHT/SpringBootHtApplication.java
@@ -0,0 +1,13 @@
+package com.jk.SpringBootHT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jk/SpringBootHT/SpringBootHtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.SpringBootHT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
