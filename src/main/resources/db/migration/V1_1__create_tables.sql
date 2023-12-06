09a6b0292a8485a4aa450fc2030dbfb66639ecb9
kajok88 <skyborgg88@gmail.com>
2023-12-06 02:58:37 +0200

    Lisätty annettu SQL-skeema T-SQL muodossa Flyway:ta varten

---
+++ src/main/resources/db/migration/V1_1__create_tables.sql
@@ -0,0 +1,54 @@
+-- Users table to store user information
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'users')
+BEGIN
+    CREATE TABLE users
+    (
+        user_id INT PRIMARY KEY IDENTITY(1,1),
+        username VARCHAR(255) NOT NULL,
+        password_hash VARCHAR(255) NOT NULL,
+        -- Add other user-related fields as needed
+        CONSTRAINT unique_username UNIQUE (username)
+    );
+END
+
+-- Categories table to store different event categories
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'categories')
+BEGIN
+    CREATE TABLE categories
+    (
+        category_id INT PRIMARY KEY IDENTITY(1,1),
+        user_id INT,
+        category_name VARCHAR(255) NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(user_id),
+        CONSTRAINT unique_category UNIQUE (user_id, category_name)
+    );
+END
+
+-- Events table to store diary entries and calendar events
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'events')
+BEGIN
+    CREATE TABLE events
+    (
+        event_id INT PRIMARY KEY IDENTITY(1,1),
+        user_id INT,
+        event_title VARCHAR(255) NOT NULL,
+        event_description TEXT,
+        event_date DATE,
+        event_time TIME,
+        due_date DATE,
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
+    );
+END
+
+-- Junction table to represent the many-to-many relationship between events and categories
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'event_categories')
+BEGIN
+    CREATE TABLE event_categories
+    (
+        event_id INT,
+        category_id INT,
+        PRIMARY KEY (event_id, category_id),
+        FOREIGN KEY (event_id) REFERENCES events(event_id),
+        FOREIGN KEY (category_id) REFERENCES categories(category_id)
+    );
+END
