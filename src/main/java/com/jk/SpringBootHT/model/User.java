6158d621bdb4a49f52aad1d6e3503fc14ee85665
kajok88 <skyborgg88@gmail.com>
2023-12-06 23:51:44 +0200

    updated

--- src/main/java/com/jk/SpringBootHT/model/User.java
+++ src/main/java/com/jk/SpringBootHT/model/User.java
@@ -1,10 +1,6 @@
 package com.jk.SpringBootHT.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "users")
