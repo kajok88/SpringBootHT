5229eae549a93a9b434519a213852e92f5083201
kajok88 <skyborgg88@gmail.com>
2024-01-07 19:39:33 +0200

    updated service layer

--- src/main/java/com/jk/SpringBootHT/service/EventService.java
+++ src/main/java/com/jk/SpringBootHT/service/EventService.java
@@ -1,10 +1,13 @@
 package com.jk.SpringBootHT.service;
 
 import com.jk.SpringBootHT.entity.Event;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 public interface EventService {
     List<Event> getAllEvents();
+    void saveEvent(Event event);
+
 
 }
--- src/main/java/com/jk/SpringBootHT/service/EventServiceImpl.java
+++ src/main/java/com/jk/SpringBootHT/service/EventServiceImpl.java
@@ -2,16 +2,24 @@ package com.jk.SpringBootHT.service;
 
 import com.jk.SpringBootHT.entity.Event;
 import com.jk.SpringBootHT.repository.IEventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 
 import java.util.List;
 
+@Service
 public class EventServiceImpl implements EventService{
 
+    @Autowired
     private IEventRepository eventRepository;
 
     @Override
     public List<Event> getAllEvents() {
         return eventRepository.findAll();
+    }
+    @Override
+    public void saveEvent(Event event) {
+        this.eventRepository.save(event);
     }
 }
