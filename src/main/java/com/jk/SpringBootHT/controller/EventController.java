2441481420e8b4a4e7f5a7a5dfaef07e67ddce02
kajok88 <skyborgg88@gmail.com>
2024-01-08 19:34:36 +0200

    updated

--- src/main/java/com/jk/SpringBootHT/controller/EventController.java
+++ src/main/java/com/jk/SpringBootHT/controller/EventController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EventController {
@@ -23,6 +25,11 @@ public class EventController {
         Event event = new Event();
         model.addAttribute("event", event);
         return "new_event";
+    }
 
+    @PostMapping("/saveEvent")
+    public String saveEvent(@ModelAttribute("event") Event event) {
+        eventService.saveEvent(event);
+        return "redirect:/";
     }
 }
