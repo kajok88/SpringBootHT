0673193dc916b47bf9296fcb3fe592e43af4c29c
kajok88 <skyborgg88@gmail.com>
2024-01-09 18:58:00 +0200

    Added functionality for finding the category by name.

--- src/main/java/com/jk/SpringBootHT/repository/ICategoryRepository.java
+++ src/main/java/com/jk/SpringBootHT/repository/ICategoryRepository.java
@@ -4,4 +4,5 @@ import com.jk.SpringBootHT.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ICategoryRepository extends JpaRepository<Category, Long> {
+    Category findByCategoryName(String categoryName);
 }
