cd7d4031317be20c0ab6e4a7a6b7692bae2e9839
kajok88 <skyborgg88@gmail.com>
2024-01-10 19:17:47 +0200

    Error handling and minor changes.

--- src/main/java/com/jk/SpringBootHT/controller/CategoryController.java
+++ src/main/java/com/jk/SpringBootHT/controller/CategoryController.java
@@ -4,12 +4,14 @@ import com.jk.SpringBootHT.entity.Category;
 import com.jk.SpringBootHT.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -25,15 +27,21 @@ public class CategoryController {
         return "new_category";
     }
     @PostMapping("addCategory")
-    public String addCategory(@RequestParam("categoryTitle") String categoryTitle, Model model) {
-        Category category = new Category();
-        category.setCategoryName(categoryTitle);
+    public String addCategory(@RequestParam("categoryTitle") String newCategoryTitle, RedirectAttributes redirectAttributes) {
+        // Tarkastetaan, löytyykö kyseinen kategoria ennestään
+        Category existingCategory = categoryService.getCategoryByName(newCategoryTitle);
+        if (existingCategory != null) {
+            // Jos löytyy, lähetetään virhe ilmoitus redirect -sivulle
+            redirectAttributes.addFlashAttribute("errorMessage", "Category already exists");
+        } else {
+            Category category = new Category();
+            category.setCategoryName(newCategoryTitle);
 
-        categoryService.saveCategory(category);
-        // Update the model with the updated list of categories
-//        List<Category> allCategories = categoryService.getAllCategories();
-//        model.addAttribute("allCategories", allCategories);
+            categoryService.saveCategory(category);
+        }
 
-        return "redirect:/showAddCategoryForm"; // Redirect to the same page
+        // Päivitetään sivu uudelleen, näyttäen muutokset
+        return "redirect:/showAddCategoryForm";
+
     }
 }
