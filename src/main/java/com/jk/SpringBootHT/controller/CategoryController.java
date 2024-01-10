4be21be8ea9299aaa67ae3607d65fb8accc70d1b
kajok88 <skyborgg88@gmail.com>
2024-01-10 18:35:24 +0200

    Minor update.

--- src/main/java/com/jk/SpringBootHT/controller/CategoryController.java
+++ src/main/java/com/jk/SpringBootHT/controller/CategoryController.java
@@ -9,16 +9,31 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
-    @PostMapping("/saveCategory")
-    public String saveCategory(@ModelAttribute("Category") Category category, Model model){
-        model.addAttribute("listCategories", categoryService.getAllCategories());
+    @GetMapping("/showAddCategoryForm")
+    public String showAddCategoryForm(Model model){
+        List<Category> allCategories = categoryService.getAllCategories();
+        model.addAttribute("allCategories", allCategories);
+        return "new_category";
+    }
+    @PostMapping("addCategory")
+    public String addCategory(@RequestParam("categoryTitle") String categoryTitle, Model model) {
+        Category category = new Category();
+        category.setCategoryName(categoryTitle);
+
         categoryService.saveCategory(category);
-        return "redirect:/";
+        // Update the model with the updated list of categories
+//        List<Category> allCategories = categoryService.getAllCategories();
+//        model.addAttribute("allCategories", allCategories);
+
+        return "redirect:/showAddCategoryForm"; // Redirect to the same page
     }
 }
