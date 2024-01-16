71062625ee0db50f582e58cea37a95591d75b5b8
kajok88 <skyborgg88@gmail.com>
2024-01-16 23:45:46 +0200

    updated AuthController

--- src/main/java/com/jk/SpringBootHT/controller/AuthController.java
+++ src/main/java/com/jk/SpringBootHT/controller/AuthController.java
@@ -1,12 +1,20 @@
 package com.jk.SpringBootHT.controller;
 
 import com.jk.SpringBootHT.entity.User;
+import com.jk.SpringBootHT.model.UserDto;
+import com.jk.SpringBootHT.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthController {
+
+    private UserService userService;
 
     // handler method to handle home page request
     @GetMapping("/login")
@@ -17,9 +25,28 @@ public class AuthController {
     // handler method to handle user registration form request
     @GetMapping("/register")
     public String showRegistrationForm(Model model){
-        // create model object to store form data
-        User user = new User();
+        UserDto user = new UserDto();
         model.addAttribute("user", user);
         return "register";
+    }
+
+    @PostMapping("/register/save")
+    public String registration(@Valid @ModelAttribute("user") UserDto userDto,
+                               BindingResult result,
+                               Model model){
+        User existingUser = userService.findByUsername(userDto.getUsername());
+
+        if(existingUser != null && existingUser.getUsername() != null){
+            result.rejectValue("username", null,
+                    "That username is already taken");
+        }
+
+        if(result.hasErrors()){
+            model.addAttribute("user", userDto);
+            return "/register";
+        }
+
+        userService.saveUser(userDto);
+        return "redirect:/register?success";
     }
 }
