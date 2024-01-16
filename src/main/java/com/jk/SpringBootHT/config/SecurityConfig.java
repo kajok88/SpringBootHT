4d7a75106152d84fe9c636976d58505c4516e956
kajok88 <skyborgg88@gmail.com>
2024-01-16 23:49:08 +0200

    added a custom filter chain

--- src/main/java/com/jk/SpringBootHT/config/SecurityConfig.java
+++ src/main/java/com/jk/SpringBootHT/config/SecurityConfig.java
@@ -2,13 +2,25 @@ package com.jk.SpringBootHT.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig{
     @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/showAddCategoryForm").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults());
+        return http.build();
     }
+
 }
